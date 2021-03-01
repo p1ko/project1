7d0eb95cc6702cbda9491c8733ce8a3a379c4fb4
Artem <artem2002shev@gmail.com>
2021-03-01 12:35:05 +0200

    firstt

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert('Hello');
+'use strict';
+
+let numberOfFilms=+prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB ={
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genders: [],
+    private: false,
+};
+
+const a=prompt('Last film','');
+const b=prompt('Mark','');
+
+personalMovieDB.movies[a]=a;
+personalMovieDB.movies[b]=b;
+
+console.log(personalMovieDB);
